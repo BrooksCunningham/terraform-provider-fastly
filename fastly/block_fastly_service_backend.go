ea8c4ee08af7c3ce42ec31ebb71cb22c92b9bf55
Brooks Cunningham <bcunningham@fastly.com>
2025-04-16 09:59:25 -0500

    Commented out "Sensitive:   true,"

--- fastly/block_fastly_service_backend.go
+++ fastly/block_fastly_service_backend.go
@@ -148,14 +148,14 @@ func (h *BackendServiceAttributeHandler) GetSchema() *schema.Schema {
 			Optional:    true,
 			Default:     "",
 			Description: "Client certificate attached to origin. Used when connecting to the backend",
-			Sensitive:   true,
+			// Sensitive:   true,
 		},
 		"ssl_client_key": {
 			Type:        schema.TypeString,
 			Optional:    true,
 			Default:     "",
 			Description: "Client key attached to origin. Used when connecting to the backend",
-			Sensitive:   true,
+			// Sensitive:   true,
 		},
 		"ssl_sni_hostname": {
 			Type:        schema.TypeString,
